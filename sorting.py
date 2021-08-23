04de8fd0ce43e8bfc0416f031d07310d5a070b1c
makxi007 <maksimilianus007@gmail.com>
2021-08-23 18:27:27 +0500

    Creating sorting automation in python

--- sorting.py
+++ sorting.py
@@ -3,26 +3,43 @@ import sys
 from os import listdir
 from os import makedirs
 
-
-# directory list
-lsdir = listdir(".")
+class VideoPhoto:
+    # jpeg, jpg, png
+    def is_photo(self, file=None):
+        file = str(file).lower()
+        if file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".png"):
+            return True
+    # mp4, mov
+    def is_video(self, file=None):
+        file = str(file).lower()
+        if file.endswith(".mp4") or file.endswith(".mov"):
+            return True
 
 # creating folder if it doesn't exist
 def create_folder(folder_name=None):
     if folder_name not in lsdir:
         makedirs(folder_name)
 
-# check if the file is video
-def is_video(file=None):
-    if file.endswith(".MOV") or file.endswith(".mov"):
-        print()
-
 # our main bro
 def main():
-    print(type(lsdir))
-    print("------------")
+    if len(sys.argv) > 2:
+        print("Too much arguments, taking only the first...")
+        print(sys.argv[1])
+        # directory list
+        lsdir = listdir(sys.argv[1])
+    else:
+        print("Taking this one...")
+        print(sys.argv[1])
+        # directory list
+        lsdir = listdir(sys.argv[1])
+
+    video_photo = VideoPhoto()
+
     for file in range(len(lsdir)):
-        print(type(file))
- 
+        if video_photo.is_video(lsdir[file]):
+            print(file, lsdir[file] , "VIDEO")
+
+        if video_photo.is_photo(lsdir[file]):
+            print(file, lsdir[file] , "PHOTO")
 
 main()
