a3bd891787c76c7551f938fa51ab5f8ea2b5ecb1
makxi007 <maksimilianus007@gmail.com>
2021-08-25 14:38:47 +0500

    Some changes

--- sorting.py
+++ sorting.py
@@ -1,7 +1,15 @@
+# sys
 import sys
-
+# shutil
+from PIL import Image, ExifTags
+# shutil
+from shutil import move
+# os
 from os import listdir
 from os import makedirs
+from os import path
+# subprocess
+import subprocess
 
 class VideoPhoto:
     # jpeg, jpg, png
@@ -24,30 +32,66 @@ def create_folder(folder_name, dir):
     else:
         print("PHOTO/VIDEO in directory")
 
+# creating subprocess and getting metadata
+def get_metadata(path_to_file):
+    info_metadata = {}
+    process = subprocess.Popen(["exiftool", path_to_file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    for tag in process.stdout:
+        line = tag.strip().split(":")
+        info_metadata[line[0].strip()] = line[-1].strip()
+
+    for k, v in info_metadata.items():
+        print(k, ":", v)
+
+    return info_metadata
+
+# saving metadata to the file
+def saving_metadata(file_txt, metadata):
+    with open(file_txt, "a") as md:
+        for k, v in metadata.items():
+            md.write(k + " : " + v + ";\n")
+
+        md.write("*****************\n")
+
 # our main bro
 def main():
+
+    metadata_file = "metadata.txt"
+    exist_ok = False
+
     if len(sys.argv) > 2:
         print("Too much arguments, taking only the first...")
         print(sys.argv[1])
         # directory list
-        lsdir = listdir(sys.argv[1])
+        dir_path = sys.argv[1]
+        lsdir = listdir(dir_path)
     else:
         print("Taking this one...")
         print(sys.argv[1])
         # directory list
-        lsdir = listdir(sys.argv[1])
+        dir_path = sys.argv[1]
+        lsdir = listdir(dir_path)
 
     video_photo = VideoPhoto()
     create_folder("VIDEO", sys.argv[1])
     create_folder("PHOTO", sys.argv[1])
 
     for file in range(len(lsdir)):
+        # is video
         if video_photo.is_video(lsdir[file]):
+            #move(f"{dir_path}/{lsdir[file]}", f"{dir_path}/VIDEO/{lsdir[file]}")
             pass
-
-
+        # is photo
         if video_photo.is_photo(lsdir[file]):
-            pass
+            metadata = get_metadata(f"{dir_path}/{lsdir[file]}")
+
+            saving_metadata(f"{dir_path}/{metadata_file}", metadata)
+
+
+
+            #move(f"{dir_path}/{lsdir[file]}", f"{dir_path}/PHOTO/{lsdir[file]}")
+
+
 
 
 main()
